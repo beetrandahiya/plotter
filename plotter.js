344e660e64ba9c793d4025572026368ac4c7fa77
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2021-01-12 14:00:14 +0530

    Added Domain , Color
    Now you can change domain of the plot and also the color of the plot.

--- plotter.js
+++ plotter.js
@@ -3,52 +3,52 @@ function plotgraph() {
     funcinp = document.getElementById("inputfunc").value;
     funcinp.trim();
     if (funcinp.includes("sin")) {                                     //sine
-        funcinp = funcinp.replace("sin", "Math.sin");
+        funcinp = funcinp.replaceAll("sin", "Math.sin");
     };
     if (funcinp.includes("cos")) {                                     //cosine
-        funcinp = funcinp.replace("cos", "Math.cos")
+        funcinp = funcinp.replaceAll("cos", "Math.cos")
     };
     if (funcinp.includes("tan")) {                                     //tangent
-        funcinp = funcinp.replace("tan", "Math.tan")
+        funcinp = funcinp.replaceAll("tan", "Math.tan")
     };
     if (funcinp.includes("^")) {                                       //exponent
-        funcinp = funcinp.replace("^", "**")
+        funcinp = funcinp.replaceAll("^", "**")
     };
     if (funcinp.includes("sqrt")) {                                     //square root
-        funcinp = funcinp.replace("sqrt", "Math.sqrt")
+        funcinp = funcinp.replaceAll("sqrt", "Math.sqrt")
     };
     if (funcinp.includes("pi")) {                                     //Pi=3.141592653589793238
-        funcinp = funcinp.replace("pi", "Math.PI")
+        funcinp = funcinp.replaceAll("pi", "Math.PI")
     };
     if (funcinp.includes("e")) {                                       //Euler's number --- e=2.71828182845904
-        funcinp = funcinp.replace("e", "Math.E")
+        funcinp = funcinp.replaceAll("e", "Math.E")
     };
     if (funcinp.includes("atan")) {                                       //tangent inverse
-        funcinp = funcinp.replace("atan", "Math.atan")
+        funcinp = funcinp.replaceAll("atan", "Math.atan")
     };
     if (funcinp.includes("acos")) {                                       //cosine inverse
-        funcinp = funcinp.replace("acos", "Math.acos")
+        funcinp = funcinp.replaceAll("acos", "Math.acos")
     };
     if (funcinp.includes("asin")) {                                       //sine inverse
-        funcinp = funcinp.replace("asin", "Math.asin")
+        funcinp = funcinp.replaceAll("asin", "Math.asin")
     };
     if (funcinp.includes("abs")) {                                       //modulus or absolute
-        funcinp = funcinp.replace("abs", "Math.abs")
+        funcinp = funcinp.replaceAll("abs", "Math.abs")
     };
     if (funcinp.includes("cbrt")) {                                       //cube root
-        funcinp = funcinp.replace("cbrt", "Math.cbrt")
+        funcinp = funcinp.replaceAll("cbrt", "Math.cbrt")
     };
     if (funcinp.includes("log")) {                                       //natural log
-        funcinp = funcinp.replace("log", "Math.log")
+        funcinp = funcinp.replaceAll("log", "Math.log")
     };
     if (funcinp.includes("ln")) {                                       //natuarl log
-        funcinp = funcinp.replace("ln", "Math.log")
+        funcinp = funcinp.replaceAll("ln", "Math.log")
     };
     if (funcinp.includes("log10")) {                                       //log to base 10
-        funcinp = funcinp.replace("log10", "Math.log10")
+        funcinp = funcinp.replaceAll("log10", "Math.log10")
     };
     if (funcinp.includes("Sgn")) {                                       //Signum function
-        funcinp = funcinp.replace("Sgn", "Math.sign")
+        funcinp = funcinp.replaceAll("Sgn", "Math.sign")
     };
 
     if (funcinp.includes("[")) {                                       //GIF -Greatest Integer Function
@@ -57,33 +57,46 @@ function plotgraph() {
         strtkn = funcinp.substring(indexstrt + 1, indexend);
         toreplace = `[${strtkn}]`;
         fnl = "Math.floor(" + strtkn + ")";
-        funcinp = funcinp.replace(toreplace, fnl);
+        funcinp = funcinp.replaceAll(toreplace, fnl);
 
 
     };
 
-    // document.getElementById("fnout").innerHTML=funcinp;
+     var dmnstart=parseInt(document.getElementById("dmnstart").value);
+     var dmnend=parseInt(document.getElementById("dmnend").value);
+     dmnstart=dmnstart*100;
+     dmnend=dmnend*100;
+
 
     var datay = [];
 
-    for (i = -1000; i < 1000; i++) {
+    for (i = dmnstart; i < dmnend; i++) {
         x = i / 100;
         yv = eval(funcinp);                                          //evaluating the function and returning y values
+        if(yv==Infinity){
+            yvu = {
+                x: x,
+                y: null
+            };
+
+        }
+        else{
         yvu = {
             x: x,
             y: yv
         };
-        datay.push(yvu);
     }
-
-    var chart = new CanvasJS.Chart("plotarea", {
+        datay.push(yvu);
+        //console.log(datay[i]);
+    }
+    
+    window.chart = new CanvasJS.Chart("plotarea", {
         animationEnabled: true,
         zoomEnabled: true,
         backgroundColor: 'rgba(255,255,255,0.3)',
         zoomType: 'xy',
         title: {
-            text: "Plotter", //change name later
-            fontFamily: "montserrat"
+            
         },
         axisX: {
             gridThickness: 0.5,
@@ -91,7 +104,7 @@ function plotgraph() {
         },
         axisY: {
             gridThickness: 0.5,
-            margin: 20,
+            margin: 20
         },
 
         data: [{
@@ -103,6 +116,17 @@ function plotgraph() {
 
     })
 
+    
     chart.render();
 
 }
+function changecolor(){
+    grphcolor=document.getElementById("color_select").value;
+    chart.options.data[0].color=grphcolor;
+    chart.render();
+
+
+
+}
+
+
